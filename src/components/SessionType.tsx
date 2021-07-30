c1d8ad084e5149f89e55e8349411ddeffc195050
Steve Lorentzen <Stevelorentzen@gmail.com>
2021-07-29 19:53:40 -0700

    refactored Sessions component.  Reduced duplicated code by creating a subcomponent

---
+++ src/components/SessionType.tsx
@@ -0,0 +1,41 @@
+import * as React from "react";
+import {Flex, Heading} from "@chakra-ui/react";
+import styled from "@emotion/styled";
+
+const SessionBox = styled(Flex)`
+  flex-direction: column;
+  width: 15%;
+  min-width: 20rem;
+  justify-content: space-between;
+  margin: 0 3rem 4rem 3rem;
+`;
+
+const SessionType: React.FC<{
+  description: string;
+  title: string;
+}> = ({children, description, title}) => {
+  return (
+    <SessionBox>
+      <Flex direction="column">
+        <Heading as="h1" fontSize="2.3rem" fontWeight="500" marginBottom="1rem">
+          <span style={{color: "var(--main-color-dark)"}}>{title}</span> Session
+        </Heading>
+        <Flex
+          display={{base: "none", sm: "flex"}}
+          direction="column"
+          fontSize="1.5rem"
+          justify="space-between"
+          marginBottom="1rem"
+        >
+          <p>{description}</p>
+        </Flex>
+      </Flex>
+
+      <Flex direction="column" justifyContent="flex-end">
+        {children}
+      </Flex>
+    </SessionBox>
+  );
+};
+
+export {SessionType};
